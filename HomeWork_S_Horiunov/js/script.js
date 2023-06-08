12bc5e464e68604bb4ac88ee022812d148e005ab
SerhiiHoriunov <serghory@gmail.com>
2023-06-08 15:51:46 +0200

    Changed files & folders

---
+++ HomeWork_S_Horiunov/js/script.js
@@ -0,0 +1,43 @@
+////////////////// Калькулятор ///////////////////
+
+// function Calculator() {
+//     this.read = function() {
+//       this.numA = +prompt("Insert number a:" , 0 );
+//       this.numB = +prompt("Insert number b:" , 0);
+//     };
+
+//     this.sum = function() {
+//       return this.numA + this.numB;
+//     };
+
+//     this.mul = function() {
+//       return this.numA * this.numB;
+//     };
+//   }
+
+/////////////////// New standart ES2015 /////////////////////
+
+// class Calculator {
+//   constructor() {
+//     this.read = function () {
+//       this.numA = +prompt("Insert number a:", 0);
+//       this.numB = +prompt("Insert number b:", 0);
+//     };
+// 1
+//     this.sum = function () {
+//       return this.numA + this.numB;
+//     };
+10
+//     this.mul = function () {
+//       return this.numA * this.numB;
+//     };
+//   }
+// }
+
+///////////////// end New standart ES2015 ////////////////
+
+  // let calc = new Calculator();
+  // calc.read();
+
+  // alert( "Sum is equal: " +  calc.sum() );
+  // alert( "Multiplication is equal: " +  calc.mul() );
